765fe4560bd72a99a5cb7547b6a9edc848f7c11f
Jerome Samson Sotto <jeromess.88@gmail.com>
2019-02-04 20:02:20 +0000

    Initialized Project

---
+++ src/main/java/com/jerome/demographic/DemographicApplication.java
@@ -0,0 +1,14 @@
+package com.jerome.demographic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographicApplication.class, args);
+	}
+
+}
+

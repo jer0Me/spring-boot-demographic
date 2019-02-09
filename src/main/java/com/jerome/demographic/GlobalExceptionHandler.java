4030263410e3598b0746c08e9f303d6200833f7a
Jerome Samson Sotto <jeromess.88@gmail.com>
2019-02-09 08:38:07 +0000

    Added logs

--- src/main/java/com/jerome/demographic/GlobalExceptionHandler.java
+++ src/main/java/com/jerome/demographic/GlobalExceptionHandler.java
@@ -1,13 +1,15 @@
 package com.jerome.demographic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     /**
      * Lets catch every unexpected exceptions and return the user to a general error page.
@@ -16,10 +18,11 @@ public class GlobalExceptionHandler {
      *
      * a proper error message to the user
      */
+
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleEmployeeNotFoundException(HttpServletRequest request, Exception ex) {
-        ModelAndView modelAndView = new ModelAndView("error_page");
-        return modelAndView;
+    public ModelAndView handleEmployeeNotFoundException(Exception ex) {
+        LOGGER.error("There was an unexpected exception", ex);
+        return new ModelAndView("error_page");
     }
 
 }
